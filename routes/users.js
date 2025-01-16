fce5fb1e1b2ce88810899cff018d7dd0040b750b
Saikat <139097941+Saikattnt@users.noreply.github.com>
2025-01-16 16:39:29 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb+srv://saikatojha0:rCLiprQHnJTQn9dH@insta-db.mk649.mongodb.net/?retryWrites=true&w=majority&appName=insta-db");
+mongoose.connect("mongodb+srv://saikatojha0:rCLiprQHnJTQn9dH@insta-db.mk649.mongodb.net/?retryWrites=true&w=majority&appName=insta-db"); //connecting it to the database
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({  //details saving in database, things user contains like username,password,images etc..
   username: String,
   name: String,
   email: String,
   password: String,
   picture: {
-    type: String,
+    type: String,     //String is used beacuse we are not stroing images we are passing url only for the image
     default: "def.png"
   },
   contact: String,
   bio: String,
   stories: [
     {
-      type: mongoose.Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,      //stroes the id of the posts rather than the posts of users
       ref: "story" 
     }
   ],
@@ -46,4 +46,4 @@ const userSchema = mongoose.Schema({
 
 userSchema.plugin(plm);
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("user", userSchema); // by this line we can create,read,update,delete data form database 
